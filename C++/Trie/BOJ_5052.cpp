fd497aa5396515167826c876341453a003a9a27b
sangil1208 <sangil1208@naver.com>
2023-02-07 22:34:53 +0900

    modify using string library (not char array)

--- C++/Trie/BOJ_5052.cpp
+++ C++/Trie/BOJ_5052.cpp
@@ -9,9 +9,16 @@ using namespace std;
 const int NUMBERS = 10;
 int toNum(char ch) { return ch - '0'; } // 숫자 0~9 저장
 
+/*
+string을 인자로 전달 시, 매 함수 호출마다 해당 값을 복사해서 사용한다.
+하지만 &을 사용해 전달하면, string을 복사하여 사용하지 않는다 (메모리 절약)
+*/
 struct TrieNode {
+
 	TrieNode* children[NUMBERS];
+
 	bool terminal;
+
 	TrieNode() : terminal(false) {
 		memset(children, 0, sizeof(children));
 	}
@@ -22,39 +29,40 @@ struct TrieNode {
 				delete children[i];
 	}
 
-	void insert(const char* key) {
-		if (*key == '\0')
+	void insert(string& key, int index) {
+		if (key[index] == '\0')
 			terminal = true;
 		else {
-			int next = toNum(*key);
+			int next = toNum(key[index]);
 			// 해당 자식 노드가 없으면 생성
 			if (children[next] == NULL)
 				children[next] = new TrieNode();
-			children[next]->insert(key + 1);
+			children[next]->insert(key, index + 1);
 		}
 	}
 
 	// key에 해당하는 문자열을 접두어로 가지고 있는지 확인하고
 	// 가지고 있다면 해당 접두어가 끝나는 부분의 위치를 반환.
-	TrieNode* find(const char* key) {
-		if (*key == '\0') return this;
-		int next = toNum(*key);
-		if (children[next] == NULL) return NULL;
-		return children[next]->find(key + 1);
+
+
+	bool find(string& key, int depth) {
+		if (key[depth] == '\0')
+			return true;
+		int next = toNum(key[depth]);
+		if (children[next] == NULL)
+			return false;
+		return children[next]->find(key, depth + 1);
 	}
 
 	// key에 해당하는 문자열이 포함되어 있는지 확인하는 코드
 	// 해당 key의 문자열이 있다면 true를 반환하고 없으면 false를 반환.
-	bool exist(const char* key) {
-		if (terminal) {
+	bool exist(string& key, int depth) {
+		if (terminal)
 			return true;
-		}
-
-		int next = toNum(*key);
-		if (children[next] == NULL) {
+		int next = toNum(key[depth]);
+		if (children[next] == NULL)
 			return false;
-		}
-		return children[next]->exist(key + 1);
+		return children[next]->exist(key, depth + 1);
 	}
 
 };
@@ -79,9 +87,9 @@ int main(void) {
 		for (register int i = 0; i < n; i++) {
 			string input;
 			cin >> input;
-			if (!root->exist(input.c_str()) && root->find(input.c_str()) == NULL) {
+			if (!root->exist(input, 0) && !root->find(input, 0)) {
 				// 동일한 문자열 포함하지 않고, INPUT이 지금까지 나온 문자열의 접두사도 아닐때
-				root->insert(input.c_str());
+				root->insert(input, 0);
 			}
 			else {
 				res = "NO";
